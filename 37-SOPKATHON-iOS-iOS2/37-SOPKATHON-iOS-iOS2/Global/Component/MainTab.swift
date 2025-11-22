b91affc87a7e757d4114000f9a53cd6b1c9d026b
KooWihC <ahnchiwook@hanmail.net>
2025-11-23 03:17:26 +0900

    [Feat] Tabbar 구현

---
+++ 37-SOPKATHON-iOS-iOS2/37-SOPKATHON-iOS-iOS2/Global/Component/MainTab.swift
@@ -0,0 +1,49 @@
+//
+//  MainTab.swift
+//  37-SOPKATHON-iOS-iOS2
+//
+//  Created by 안치욱 on 11/23/25.
+//
+
+
+import UIKit
+
+enum MainTab: CaseIterable {
+    case home
+    case search
+    case save
+}
+
+extension MainTab {
+    var title: String {
+        switch self {
+        case .home: return "홈"
+        case .search: return "검색"
+        case .save: return "저장"
+        }
+    }
+    
+    var defaultIcon: UIImage? {
+        switch self {
+        case .home: return .homeDefault
+        case .search: return .searchDefault
+        case .save: return .saveDefault
+        }
+    }
+    
+    var selectedIcon: UIImage? {
+        switch self {
+        case .home: return .homeSelected
+        case .search: return .searchSelected
+        case .save: return .saveSelected
+        }
+    }
+    
+    var viewController: UIViewController {
+        switch self {
+        case .home: return HomeViewController()
+        case .search: return SearchViewController()
+        case .save: return SaveViewController()
+        }
+    }
+}
