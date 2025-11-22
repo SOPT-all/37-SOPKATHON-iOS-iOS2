900960cd1218f2db008278b0d44fd1046badbbc4
JaeSunEo <wotjs020708@gmail.com>
2025-11-23 04:55:16 +0900

    [feat] StackView에 넣음

--- 37-SOPKATHON-iOS-iOS2/37-SOPKATHON-iOS-iOS2/Presentation/Search/View/SearchCell.swift
+++ 37-SOPKATHON-iOS-iOS2/37-SOPKATHON-iOS-iOS2/Presentation/Search/View/SearchCell.swift
@@ -17,7 +17,7 @@ final class SearchCell: UITableViewCell, ReuseIdentifiable {
     //MARK: - UI
     private lazy var titleImage = UIImageView()
     private let titleLabel = UILabel()
-    
+    private let titleStackView = UIStackView()
     
     //MARK: - init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -40,7 +40,7 @@ final class SearchCell: UITableViewCell, ReuseIdentifiable {
     //MARK: - UI
     
     func setUI() {
-        addSubviews(titleImage, titleLabel)
+        addSubviews(titleStackView)
     }
     
     func setStyle() {
@@ -54,19 +54,20 @@ final class SearchCell: UITableViewCell, ReuseIdentifiable {
             $0.text = "제목"
             $0.textColor = .suntaBlack
             $0.font = .pretendard(.body_sb_14)
-            
+        }
+        
+        titleStackView.do {
+            $0.addArrangedSubviews(titleImage,titleLabel)
+            $0.axis = .horizontal
+            $0.alignment = .leading
+            $0.spacing = 12
         }
     }
     
     func setLayout() {
-        titleImage.snp.makeConstraints {
-            $0.bottom.leading.top.equalToSuperview()
-            $0.size.equalTo(70)
-            
-        }
-        titleLabel.snp.makeConstraints{
-            $0.leading.equalTo(titleImage.snp.trailing).offset(12)
-            $0.centerY.equalToSuperview()
+        titleStackView.snp.makeConstraints {
+            $0.bottom.top.equalToSuperview().inset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
         }
         
     }
